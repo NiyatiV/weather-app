86a3219a5a2d7484c56cf556a85d1be87416666a
NiyatiV <66249087+NiyatiV@users.noreply.github.com>
2022-09-05 00:29:37 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const apiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a61c9fa2b0mshf48743570ab38b9p1c2db4jsn3aab105eaab2',
+		'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
+	}
+};
+
+export const API_URL_WEATHER = "https://yahoo-weather5.p.rapidapi.com/weather"
